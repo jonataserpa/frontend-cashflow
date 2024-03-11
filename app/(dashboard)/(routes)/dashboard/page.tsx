9e4644171282ccd08f3d9fc8f0fb33389988567f
Jonata Serpa <jonataserpa@gmail.com>
2024-03-11 09:12:16 -0300

    fix: add chart

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,7 +2,14 @@
 
 import { ICashFlowProps } from "@/app/(cash)/(routes)/cash/interfaces/iCashFlow.interface";
 import { useRouter } from "next/navigation";
-import { ArrowRight, ScreenShare } from "lucide-react";
+import {
+  ArrowDownFromLineIcon,
+  ArrowUpFromLineIcon,
+  AlignStartVerticalIcon,
+  ScreenShare,
+} from "lucide-react";
+import Chart from "@/components/chart";
+import BarChart from "@/components/chart";
 
 export type IHomeProps = {
   rows?: ICashFlowProps[];
@@ -38,14 +45,73 @@ const HomePage = ({ rows }: any) => {
           Listagem das entradas e saídas
         </p>
       </div>
+
       <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        <div className="flex flex-row gap-x-4 ...">
+          <div
+            onClick={() => router.push("/cash")}
+            key="entry"
+            className="p-4 border-black/5 border-r-4 ring-1 rounded-lg flex items-center justify-between hover:shadow-md transition cursor-pointer"
+            id="entry"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className="p-2 w-fit rounded-md bg-violet-500/10">
+                <ArrowDownFromLineIcon className="w-8 h-8 text-green-700" />
+              </div>
+              <div className="flex flex-col ...">
+                <div className="font-semibold">
+                  Entrada: <label className="text-neutral-500">R$ 5000</label>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            onClick={() => router.push("/cash")}
+            key="exit"
+            className="p-4 border-black/5 border-r-4 ring-1 rounded-lg flex items-center justify-between hover:shadow-md transition cursor-pointer"
+            id="exit"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className="p-2 w-fit rounded-md bg-violet-500/10">
+                <ArrowUpFromLineIcon className="w-8 h-8 text-red-700" />
+              </div>
+              <div className="flex flex-col ...">
+                <div className="font-semibold">
+                  Saída: <label className="text-neutral-500">R$ 7000</label>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div
+            onClick={() => router.push("/cash")}
+            key="exit"
+            className="p-4 border-black/5 border-r-4 ring-1 rounded-lg flex items-center justify-between hover:shadow-md transition cursor-pointer"
+            id="exit"
+          >
+            <div className="flex items-center gap-x-4">
+              <div className="p-2 w-fit rounded-md bg-violet-500/10">
+                <AlignStartVerticalIcon className="w-8 h-8 text-violet-500" />
+              </div>
+              <div className="flex flex-col ...">
+                <div className="font-semibold">
+                  Total: <label className="text-neutral-500">R$ -2000</label>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <BarChart />
+
         {rows && rows.length === 0 && (
           <div className="text-muted-foreground font-light text-sm md:text-lg text-center">
             <strong>Carregando...</strong>
           </div>
         )}
 
-        {rows &&
+        {/*rows &&
           rows.map((tool: ICashFlowProps) => (
             <div
               onClick={() => router.push("/cash")}
@@ -64,7 +130,9 @@ const HomePage = ({ rows }: any) => {
                   </div>
                   <div className="font-semibold">
                     Observação:
-                    <label className="text-neutral-500">{tool.observation}</label>
+                    <label className="text-neutral-500">
+                      {tool.observation}
+                    </label>
                   </div>
                   <div className="font-semibold">
                     Descrição: {validateColorText(tool.description)}
@@ -73,7 +141,7 @@ const HomePage = ({ rows }: any) => {
               </div>
               <ArrowRight className="w-5 h-5" />
             </div>
-          ))}
+          ))*/}
       </div>
     </div>
   );
---
+++ components/chart.tsx
@@ -0,0 +1,57 @@
+import { useEffect } from "react";
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
+
+function BarChart() {
+  useEffect(() => {
+   const context = (document.getElementById("myChart") as HTMLFormElement);
+    var ctx = context.getContext("2d");
+
+    const myChart = Chart.getChart("myChart");
+    if (myChart) {
+        myChart.destroy();
+    }
+      
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: [
+          "Janeiro",
+          "Fevereiro",
+          "Março",
+        ],
+        datasets: [
+          {
+            data: [66, 144],
+            label: "Entrada",
+            borderColor: "rgb(109, 253, 181)",
+            backgroundColor: "rgb(109, 253, 181,0.5)",
+            borderWidth: 2,
+          },
+          {
+            data: [20, 24],
+            label: "Saída",
+            borderColor: "rgb(255, 205, 86)",
+            backgroundColor: "rgb(255, 205, 86,0.5)",
+            borderWidth: 2,
+          },
+        ],
+      },
+    });
+  }, []);
+
+
+
+  return (
+    <>
+      <div className="h-screen">
+        <div className="border border-gray-400 pt-0 rounded-xl  w-full h-fit my-auto  shadow-xl">
+          <canvas id="myChart"></canvas>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default BarChart;
