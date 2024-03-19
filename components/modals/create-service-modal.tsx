b37e35b7f06b2ea1fc9bd1ef3c758f54f9f50190
Jonata Serpa <jonataserpa@gmail.com>
2024-03-19 13:56:24 -0300

    fix: adjust save cad

--- components/modals/create-service-modal.tsx
+++ components/modals/create-service-modal.tsx
@@ -197,7 +197,6 @@ export const CreateServiceModal = () => {
                                         <FormControl>
                                             <NumericFormat
                                                 {...field}
-                                                disabled={isLoading}
                                                 className={cn(
                                                     "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
                                                     "bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0",
@@ -264,7 +263,6 @@ export const CreateServiceModal = () => {
                                     <FormItem>
                                         <FormLabel>Tipo</FormLabel>
                                         <Select
-                                            disabled={isLoading}
                                             onValueChange={field.onChange}
                                             data-testid="type"
                                             name="type"
@@ -293,11 +291,7 @@ export const CreateServiceModal = () => {
                             />
                         </div>
                         <DialogFooter className="bg-gray-100 px-6 py-4">
-                            <Button
-                                variant="default"
-                                disabled={isLoading}
-                                data-testid="save"
-                            >
+                            <Button variant="default" data-testid="save">
                                 Salvar
                             </Button>
                         </DialogFooter>
