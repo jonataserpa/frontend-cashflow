88bb10e66578615f2953db317fd40014309d63ac
Jonata Serpa <jonataserpa@gmail.com>
2024-03-21 09:49:19 -0300

    fix: adjust ci

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
-import { useEffect } from "react";
 import { AuthService } from "./services/auth-services";
 
 export const config = {
-  matcher: "/((?!_next/static|_next/image|favicon.ico).*)",
+    matcher: "/((?!_next/static|_next/image|favicon.ico).*)",
 };
 
 const publicRoutes = ["/login"];
 
 export async function middleware(req: NextRequest) {
-  const pathname = req.nextUrl.pathname;
+    const pathname = req.nextUrl.pathname;
 
-  if (publicRoutes.includes(pathname)) {
-    return NextResponse.next();
-  }
+    if (publicRoutes.includes(pathname)) {
+        return NextResponse.next();
+    }
 
-  const authService = new AuthService();
-  const user = authService.getUser();
-  if (authService.isTokenExpired() || !user) {
-    const { pathname } = new URL(req.url);
-
-    return NextResponse.redirect(
-      new URL(`/login`, req.url)
-    );
-  }
+    const authService = new AuthService();
+    const user = authService.getUser();
+    if (authService.isTokenExpired() || !user) {
+        return NextResponse.redirect(new URL(`/login`, req.url));
+    }
 }
